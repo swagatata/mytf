034c469eb48ad2b0fa62c7c276a0d7d144b368f1
Swagat Konchada <swagatata@gmail.com>
2018-10-08 14:41:07 +0530

    removing the failure scenario

--- simple.tf
+++ simple.tf
@@ -3,12 +3,6 @@ resource "local_file" "foo" {
     content  = "${var.content}"
     filename = "/Users/swagatkonchada/foo.bar"
 }
-
-resource "local_file" "another_foo" {
-    content  = "${var.content}"
-    filename = "/usr/bin/something"
-}
-
 output "region" {
     value = "us-east-1"
 }
