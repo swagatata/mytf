7be8b4f7e3668fbe7b5cdbbba7db9576cd73affc
Swagat Konchada <swagatata@gmail.com>
2018-10-18 03:18:55 +0530

    Update simple.tf

--- simple.tf
+++ simple.tf
@@ -1,6 +1,8 @@
 variable "content" {}
+variable "more_content" {}
+
 resource "local_file" "foo" {
-    content  = "${var.content}"
+    content  = "${var.content} ${var.more_content}"
     filename = "/Users/swagatkonchada/car.foo"
 }
 output "region" {
